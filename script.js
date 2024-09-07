7db8d9efdee13cfc323f857550721b04e8b57991
Lucas Rial <111540106+LucasRialx@users.noreply.github.com>
2024-09-07 20:59:11 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navbar = document.querySelector('.navbar');
-
-    menuToggle.addEventListener('click', function() {
-        navbar.classList.toggle('active');
-    });
-});
